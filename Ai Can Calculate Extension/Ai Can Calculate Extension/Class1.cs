37527bf1c79ac6ad4f3d732812b531755db5fe08
Tony-Chau <tonychau923@gmail.com>
2019-04-04 14:59:15 +1000

    Added solution for custom

---
+++ Ai Can Calculate Extension/Ai Can Calculate Extension/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ai_Can_Calculate_Extension
+{
+    public class StringToCsharpCode : CodeActivity
+    {
+
+        [Category("Input")]
+        public InArgument<string> Str { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            string str = Str.Get(context);
+            //add
+            str = str.Replace("add", "+");
+            str = str.Replace("plus", "+");
+
+            //minus
+            str = str.Replace("minus", "-");
+            str = str.Replace("subtract", "-");
+
+            //multiply
+            str = str.Replace("times", "*");
+            str = str.Replace("multiply by", "*");
+
+            //divide
+            str = str.Replace("divde", "/");
+            str = str.Replace("divide by", "/");
+            str = str.Replace("over", "/");
+
+            //Executable code here
+
+        }
+    }
+}
