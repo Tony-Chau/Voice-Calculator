c9ded39d7d4c611a1ea0d164ff996c1b56618513
Tony-Chau <tonychau923@gmail.com>
2019-04-07 17:27:33 +1000

    Made some major changes
    removed unwanted files
    added word sort activity
    Re-arrange some activities within RPA

--- AiCanCalculate/AiCanCalculate/Class1.cs
+++ AiCanCalculate/AiCanCalculate/Class1.cs
@@ -23,4 +23,37 @@ namespace AiCanCalculate
             synth.Speak(Message.Get(context));
         }
     }
+
+    public class WordSort : CodeActivity
+    {
+        [Category("Input")]
+        public InArgument<string> Message { get; set; }
+
+        [Category("Output")]
+        public OutArgument<string> Response { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            string message = Message.Get(context);
+            if (message.Contains("computer what is "))
+            {
+                try
+                {
+                    int messagesize = ("computer what is ").Length;
+                    int index = message.IndexOf("computer what is ") + messagesize;
+                    int length = message.Length - messagesize;
+                    string real = message.Substring(index, length);
+                    Response.Set(context, real);
+                    return;
+                }
+                catch (Exception ex)
+                {
+                    Response.Set(context, "");
+                    return;
+                }
+            }
+            Response.Set(context, "");
+
+        }
+    }
 }
